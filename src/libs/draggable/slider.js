6999f2ef00526d686dea9a5da0a2c6e306479b67
alexandrzhyliuk <alexzhyliuk3@gmail.com>
2025-01-22 18:47:20 +0300

    mortgage

--- src/libs/draggable/slider.js
+++ src/libs/draggable/slider.js
@@ -3,26 +3,26 @@ try {
     let mouseDown = false;
     let isDragging = false;
     let startX, scrollLeft;
-    const slider = document.querySelector('.draggable');
+    const slider = $('.draggable');
     
-    const startDragging = (e) => {
+    const startDragging = function(e) {
         mouseDown = true;
         isDragging = false; // сбрасываем флаг перетаскивания
-        startX = e.pageX - slider.offsetLeft;
-        scrollLeft = slider.scrollLeft;
+        startX = e.pageX - this.offsetLeft;
+        scrollLeft = this.scrollLeft;
     }
     
-    const stopDragging = (e) => {
+    const stopDragging = function(e) {
         mouseDown = false;
     }
     
-    const move = (e) => {
+    const move = function(e) {
         e.preventDefault();
         if (!mouseDown) { return; }
         
-        const x = e.pageX - slider.offsetLeft;
+        const x = e.pageX - this.offsetLeft;
         const scroll = x - startX;
-        slider.scrollLeft = scrollLeft - scroll;
+        this.scrollLeft = scrollLeft - scroll;
         
         if (Math.abs(scroll) > 5) { // если движение больше 5px, считаем, что началось перетаскивание
             isDragging = true;
@@ -30,7 +30,7 @@ try {
     }
     
     // Предотвращаем переход по ссылке, если было перетаскивание
-    slider.querySelectorAll('a').forEach(link => {
+    slider.find('a').each(function(i, link) {
         link.addEventListener('click', (e) => {
             if (isDragging) {
                 e.preventDefault(); // отменяем переход по ссылке
@@ -39,10 +39,10 @@ try {
     });
     
     // Добавляем обработчики событий
-    slider.addEventListener('mousemove', move, false);
-    slider.addEventListener('mousedown', startDragging, false);
-    slider.addEventListener('mouseup', stopDragging, false);
-    slider.addEventListener('mouseleave', stopDragging, false);
+    slider.on('mousemove', move);
+    slider.on('mousedown', startDragging);
+    slider.on('mouseup', stopDragging);
+    slider.on('mouseleave', stopDragging);
     
 } catch {
 
